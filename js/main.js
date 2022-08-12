e4046a2c91835e47e591f9562783409a3051fd09
Wicar Pessoa <wicar_pessoa@hotmail.com>
2022-08-11 23:02:59 -0300

    testing styles

--- js/main.js
+++ js/main.js
@@ -2,3 +2,17 @@ import { FavoritesView } from "./Favorites.js";
 
 new FavoritesView("#app");
 
+const input = document.querySelector(".search input"); // get the input element
+input.addEventListener("resize", resizeInput); // bind the "resizeInput" callback on "input" event
+resizeInput.call(input); // immediately call the function
+
+function resizeInput() {
+  console.log(document.documentElement.clientWidth);
+  console.dir(input);
+  console.log(this.placeholder);
+
+  if (document.documentElement.clientWidth <= 700) {
+    this.style.width = "20rem";
+    this.placeholder = "aaa";
+  }
+}
